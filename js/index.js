2dfb7f62033a03e80219910413dbf175e41249db
ocugamer <emilocumarez@gmail.com>
2026-05-20 14:56:19 -0400

    mensaje de whatsapp

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@
 document.getElementById('btn').addEventListener("click" , ()=>{
     window.open("https://wa.me/+18294943457" , "_blank")
 })
+
+document.getElementById('btn2').addEventListener("click", ()=>{
+    window.open("https://wa.me/+18492451759?text=Hola%20quiero%20más%20información", "_blank")
+})
